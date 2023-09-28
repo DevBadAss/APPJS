69dd5302f8f69c29dc372ec9078254707e59c61a
Emmanuel Olawoore <101262045+DevBadAss@users.noreply.github.com>
2023-09-28 10:32:39 +0100

    Update blazeview.js

--- src/plugins/BlazeView/blazeview.js
+++ src/plugins/BlazeView/blazeview.js
@@ -5,73 +5,59 @@
  * @author Olawoore Emmanuel Collins
  * @link https://github.com/devbadass
  */
-class BlazeView
- */
 class BlazeView {
-  /**
-   * Render a template with the provided data.
-   *
-   * @static
-   * @param {string} template - The template string.
-   * @param {Object} data - The data object to populate the template.
-   * @returns {string} The rendered template.
-   * @throws {Error} If the template contains syntax errors.
-   * @example
-   * const template = "Hello, {{name}}! {{if age}}You are {{age}} years old.{{endif}}";
-   * const data = { name: "Alice", age: 30 };
-   * const rendered = BlazeView.render(template, data);
-   * console.log(rendered); // Output: "Hello, Alice! You are 30 years old."
-   */
-  static render(template, data) {
-    try {
-      // Handle variables
-      const renderedTemplate = template.replace(/\{\{(.*?)\}\}/g, (_, key) => {
-        if (data.hasOwnProperty(key)) {
-          return data[key];
-        } else {
-          return '';
+    /**
+     * Render a template with the provided data.
+     *
+     * @static
+     * @param {string} template - The template string.
+     * @param {Object} data - The data object to populate the template.
+     * @returns {string} The rendered template.
+     * @throws {Error} If the template contains syntax errors or if data is missing.
+     * @example
+     * const template = "Hello, {{name}}! {{if age}}You are {{age}} years old.{{endif}}{{foreach friends}}{{name}} is your friend.{{endforeach}}";
+     * const data = {
+     *   name: "Alice",
+     *   age: 30,
+     *   friends: [
+     *     { name: "Bob" },
+     *     { name: "Charlie" },
+     *   ],
+     * };
+     * const rendered = BlazeView.render(template, data);
+     * console.log(rendered);
+     */
+    static render(template, data) {
+        try {
+            let renderedTemplate = template;
+
+            // Handle conditionals
+            renderedTemplate = renderedTemplate.replace(/\{\{if (.*?)\}\}(.*?)\{\{endif\}\}/gs, (match, condition, content) => {
+                return data[condition.trim()] ? content : '';
+            });
+
+            // Handle loops
+            renderedTemplate = renderedTemplate.replace(/\{\{foreach (.*?)\}\}(.*?)\{\{endforeach\}\}/gs, (match, arrayName, content) => {
+                if (Array.isArray(data[arrayName.trim()])) {
+                    return data[arrayName.trim()]
+                        .map(item => BlazeView.render(content, {...data, ...item }))
+                        .join('');
+                } else {
+                    return '';
+                }
+            });
+
+            // Handle variables
+            renderedTemplate = renderedTemplate.replace(/\{\{(\w+)\}\}/g, (match, keyword) => {
+                return data[keyword.trim()] || '';
+            });
+
+            return renderedTemplate;
+        } catch (error) {
+            throw new Error('Template rendering failed: ' + error.message);
         }
-      });
-
-      // Handle conditionals
-      const withConditionals = renderedTemplate.replace(/\{\{if (.*?)\}\}(.*?)\{\{endif\}\}/gs, (match, condition, content) => {
-        return data[condition] ? content : '';
-      });
-
-      // Handle loops
-      const withLoops = withConditionals.replace(/\{\{foreach (.*?)\}\}(.*?)\{\{endforeach\}\}/gs, (match, arrayName, content) => {
-        if (Array.isArray(data[arrayName])) {
-          return data[arrayName].map(item => BlazeView.render(content, { ...item, ...data })).join('');
-        } else {
-          return '';
-        }
-      });
-
-      return withLoops;
-    } catch (error) {
-      throw new Error('Template rendering failed: ' + error.message);
     }
-  }
 }
-
-
-// const template = "Hello, {{name}}! {{if age}}You are {{age}} years old.{{endif}}{{foreach friends}}{{name}} is your friend.{{endforeach}}";
-// const data = {
-//   name: "Alice",
-//   age: 30,
-//   friends: [
-//     { name: "Bob" },
-//     { name: "Charlie" },
-//   ],
-// };
-
-// try {
-//   const rendered = BlazeView.render(template, data);
-//   console.log(rendered);
-// } catch (error) {
-//   console.error(error.message);
-// }
-
 
 export default BlazeView;
 
